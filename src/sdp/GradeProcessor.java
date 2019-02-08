bb70b3c9c54ee523c1a2c9b0a0727ff9f3196dd8
Haris Skiadas <cskiadas@gmail.com>
2019-02-07 22:58:02 -0500

    Extract method from switch statement.

--- src/sdp/GradeProcessor.java
+++ src/sdp/GradeProcessor.java
@@ -19,48 +19,7 @@ class GradeProcessor {
       if (scanner.hasNextLine()) {
         scanner.nextLine();
       }
-      double gradePointsForLetter;
-      switch (letterGrade) {
-      case "A":
-        gradePointsForLetter = 4.00;
-        break;
-      case "A-":
-        gradePointsForLetter = 3.67;
-        break;
-      case "B+":
-        gradePointsForLetter = 3.33;
-        break;
-      case "B":
-        gradePointsForLetter = 3.00;
-        break;
-      case "B-":
-        gradePointsForLetter = 2.67;
-        break;
-      case "C+":
-        gradePointsForLetter = 2.33;
-        break;
-      case "C":
-        gradePointsForLetter = 2.00;
-        break;
-      case "C-":
-        gradePointsForLetter = 1.67;
-        break;
-      case "D+":
-        gradePointsForLetter = 1.33;
-        break;
-      case "D":
-        gradePointsForLetter = 1.00;
-        break;
-      case "F":
-        gradePointsForLetter = 0.00;
-        break;
-      case "W":
-        gradePointsForLetter = 0.00;
-        break;
-      default:
-        throw new RuntimeException();
-      }
-      this.totalGradePoints += gradePointsForLetter;
+      totalGradePoints += getGradePointsForLetter(letterGrade);
       if (!letterGrade.equals("W")) {
         courseCount += 1;
       }
@@ -68,5 +27,50 @@ class GradeProcessor {
     double gradePointAverage = courseCount == 0 ? 0 : totalGradePoints / courseCount;
 
     return String.format("Courses: %d\nGPA: %.2f\n", courseCount, gradePointAverage);
+  }
+
+  private double getGradePointsForLetter(String letterGrade) {
+    double gradePointsForLetter;
+    switch (letterGrade) {
+    case "A":
+      gradePointsForLetter = 4.00;
+      break;
+    case "A-":
+      gradePointsForLetter = 3.67;
+      break;
+    case "B+":
+      gradePointsForLetter = 3.33;
+      break;
+    case "B":
+      gradePointsForLetter = 3.00;
+      break;
+    case "B-":
+      gradePointsForLetter = 2.67;
+      break;
+    case "C+":
+      gradePointsForLetter = 2.33;
+      break;
+    case "C":
+      gradePointsForLetter = 2.00;
+      break;
+    case "C-":
+      gradePointsForLetter = 1.67;
+      break;
+    case "D+":
+      gradePointsForLetter = 1.33;
+      break;
+    case "D":
+      gradePointsForLetter = 1.00;
+      break;
+    case "F":
+      gradePointsForLetter = 0.00;
+      break;
+    case "W":
+      gradePointsForLetter = 0.00;
+      break;
+    default:
+      throw new RuntimeException();
+    }
+    return gradePointsForLetter;
   }
 }
