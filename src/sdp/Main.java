37cae6cc547df8a44bacec79df0adc322cfcddfd
Haris Skiadas <cskiadas@gmail.com>
2019-02-08 22:18:32 -0500

    Move tests to separate directory and classes.

--- src/sdp/Main.java
+++ src/sdp/Main.java
@@ -2,67 +2,15 @@ package sdp;
 
 import java.util.Scanner;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-
 public class Main {
-
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.println(processGrades(scanner));
   }
 
-  private static String processGrades(Scanner scanner) {
+  protected static String processGrades(Scanner scanner) {
     GradeProcessor gradeProcessor = new GradeProcessor();
     gradeProcessor.addGrades(new GradeLineParser(scanner));
     return gradeProcessor.prepareReport();
   }
-
-  @Test
-  public void emptyGradeListReportsZeroGpaAndNoCourses() {
-    assertTotalsOfList_Are("", "Courses: 0\nGPA: 0.00\n");
-  }
-
-  @Test
-  public void singleCourseGradeReportsItself() {
-    assertTotalsOfList_Are("CS 234        A ", "Courses: 1\nGPA: 4.00\n");
-    assertTotalsOfList_Are("MAT    234    A-", "Courses: 1\nGPA: 3.67\n");
-    assertTotalsOfList_Are("ENGR 121      B+", "Courses: 1\nGPA: 3.33\n");
-    assertTotalsOfList_Are("CS 234         B", "Courses: 1\nGPA: 3.00\n");
-    assertTotalsOfList_Are("CS 234        B-", "Courses: 1\nGPA: 2.67\n");
-    assertTotalsOfList_Are("ENGR 011     C+ ", "Courses: 1\nGPA: 2.33\n");
-    assertTotalsOfList_Are("ENGR 101      C ", "Courses: 1\nGPA: 2.00\n");
-    assertTotalsOfList_Are("ENGR 101    C-\n", "Courses: 1\nGPA: 1.67\n");
-    assertTotalsOfList_Are("ENGR 101      D+", "Courses: 1\nGPA: 1.33\n");
-    assertTotalsOfList_Are("CS 122L        D", "Courses: 1\nGPA: 1.00\n");
-    assertTotalsOfList_Are("CS 122L        F", "Courses: 1\nGPA: 0.00\n");
-  }
-
-  @Test
-  public void emptySpaceLinesAtEndAreHandled() {
-    assertTotalsOfList_Are("CS 234        A \n  ", "Courses: 1\nGPA: 4.00\n");
-  }
-    @Test
-  public void multipleCoursesAreAllComputed() {
-    assertTotalsOfList_Are(
-            "CS 234        A \nMAT 111      B",
-            "Courses: 2\nGPA: 3.50\n");
-    assertTotalsOfList_Are(
-            "CS 234        A \nMAT 111      B\nCS 122     B-\n",
-            "Courses: 3\nGPA: 3.22\n");
-
-  }
-
-  @Test
-  public void withdrawnCoursesDontCountTowardsCoursesTaken() {
-    assertTotalsOfList_Are("CS 122L      W", "Courses: 0\nGPA: 0.00\n");
-    assertTotalsOfList_Are(
-            "CS 234        A \nMAT 111      B\nCS 122     W\n",
-            "Courses: 2\nGPA: 3.50\n");
-  }
-
-  private void assertTotalsOfList_Are(String input, String output) {
-    assertEquals(output, processGrades(new Scanner(input)));
-  }
-
 }
