ec6b65bd5c8d1fe352b3ea0a51ac0c15ce37ad9a
Haris Skiadas <cskiadas@gmail.com>
2019-02-09 17:44:17 -0500

    Vertically align codes and grades on course list printing

--- src/sdp/CourseListPrinter.java
+++ src/sdp/CourseListPrinter.java
@@ -5,9 +5,17 @@ import java.util.List;
 
 public class CourseListPrinter {
   private List<Course> courses;
+  private String formatString = "%s %s %s\n";
 
   public CourseListPrinter(List<Course> courses) {
     this.courses = courses;
+    determineFormatList();
+  }
+
+  private void determineFormatList() {
+    formatString =
+            "%-" + getMaxPrefixLength() + "s " +
+            "%-" + getMaxCodeLength() + "s %s\n";
   }
 
   public String reportCourseList() {
@@ -17,7 +25,6 @@ public class CourseListPrinter {
     }
     return sb.toString();
   }
-
   private ArrayList<Course> getAlphabeticallySortedCourses() {
     ArrayList<Course> sortedCourses = new ArrayList<>(courses);
     sortedCourses.sort(Course.alphabeticalComparator());
@@ -25,6 +32,19 @@ public class CourseListPrinter {
   }
 
   private String formattedCourse(Course course) {
-    return String.format("%s %s %s\n", course.deptPrefix, course.courseCode, course.grade);
+    return String.format(formatString, course.deptPrefix, course.courseCode, course.grade);
   }
+
+  private int getMaxPrefixLength() {
+    return courses.stream()
+            .map( c -> c.deptPrefix.length())
+            .reduce(0, (x,y) -> Integer.max(x,y));
+  }
+
+  private int getMaxCodeLength() {
+    return courses.stream()
+            .map( c -> c.courseCode.length())
+            .reduce(0, (x,y) -> Integer.max(x,y));
+  }
+
 }
--- test/sdp/CourseListPrinterTest.java
+++ test/sdp/CourseListPrinterTest.java
@@ -23,6 +23,23 @@ public class CourseListPrinterTest {
             "CS 021C B+\nCS 121C B+\nCS 131L C-\n");
   }
 
+  @Test
+  public void resultAddsSpacesToPropertyVerticallyAlignColumns() {
+    assertAddedCoursesReportedAlphabetically(
+            List.of(new Course("CS", "121C", Grade.BPLUS),
+                    new Course("MAT", "121C", Grade.B)),
+            "CS  121C B+\n" +
+                    "MAT 121C B\n");
+    assertAddedCoursesReportedAlphabetically(
+            List.of(new Course("CS", "121L1", Grade.BPLUS),
+                    new Course("MAT", "121C", Grade.B),
+    new Course("HF", "101", Grade.AMINUS)),
+            "CS  121L1 B+\n" +
+                    "HF  101   A-\n" +
+                    "MAT 121C  B\n");
+
+  }
+
   private void assertAddedCoursesReportedAlphabetically(List<Course> courses, String output) {
     CourseProcessor processor = new CourseProcessor();
     processor.addCourses(courses);
